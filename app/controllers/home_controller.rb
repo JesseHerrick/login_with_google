3539c46e66b25199db525a5b4e6137d1d0a570a0
Jesse Herrick <jessegrantherrick@gmail.com>
2015-04-12 17:26:29 -0400

    Remove home controller => pages controller

--- app/controllers/home_controller.rb
+++
@@ -1,4 +0,0 @@
-class HomeController < ApplicationController
-  def secure
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/secure'
+
   get 'home/secure'
 
 end
